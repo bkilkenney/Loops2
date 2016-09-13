9166ce59be7e2b70556b5a1d4c50293c0b228d08
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-13 14:54:58 -0400

    foreach

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@ namespace Loops2_InfiniteLoops
     {
         static void Main(string[] args)
         {
+
+            //For study session. Do-While were stuck on
+
             //Console.WriteLine("Enter a number.");
             //int n = int.Parse(Console.ReadLine());
             //Console.WriteLine("Enter another number.");
@@ -40,19 +43,85 @@ namespace Loops2_InfiniteLoops
             //}
 
 
-            Console.WriteLine("Enter a number.");
-            int userNumber = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Enter a larger number as an exponent.");
-            int userExponent = int.Parse(Console.ReadLine());
 
-            int result = 1;
+            //for Loop
 
-           for(int i = 0; i < userExponent; i++)
+            // Console.WriteLine("Enter a number.");
+            // int userNumber = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine("Enter a larger number as an exponent.");
+            // int userExponent = int.Parse(Console.ReadLine());
+
+            // int result = 1;
+
+            //for(int i = 0; i < userExponent; i++)
+            // {
+            //     userNumber *= userExponent;
+            // }
+            // Console.WriteLine(userNumber);
+
+
+
+            //Continue.  
+
+
+            //sums all odd numbers 1 to n.
+            //except if number is evenly divisible by 7
+
+            //int n = int.Parse(Console.ReadLine());
+            //int sum = 0;
+            //for (int i = 1; i <= n; i += 2)
+            //{
+            //    if (i % 7 == 0)
+            //    {
+            //        continue;
+            //    }
+            //    sum += i;
+            //}
+            //Console.WriteLine("sum = " + sum);
+
+
+
+
+            ////sum even numbers unless number is divisible by 6
+            //            int n = int.Parse(Console.ReadLine());
+            //            int sum = 0;
+
+            //            for (int i = 1; i <= n; i += 2)
+            //            {
+            //                if (i % 6 == 0)
+            //                {
+            //                    continue;
+            //                }
+
+            //            sum += i;
+            //            }
+
+            //            Console.WriteLine("sum = " + sum);
+
+
+
+            //For each loops
+            //string[] cities = { "New York", "London", "Paris" };
+
+            //foreach(string newStringCity in cities)
+            //{
+            //    Console.WriteLine("I love " + newStringCity);
+            //}
+
+
+
+
+
+
+
+            string[] favFoods = { "Nestle", "Lamb", "Pizza" };
+
+            foreach (string individualFoods in favFoods)
             {
-                userNumber *= userExponent;
+                Console.WriteLine("I love " + individualFoods);
             }
-            Console.WriteLine(userNumber);
 
         }
     }
