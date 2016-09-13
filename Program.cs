6d1675ddd77e6a27f4be1830330065cfaa9b2ace
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-13 13:51:32 -0400

    Second Commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops2_InfiniteLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter a number.");
+            //int n = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter another number.");
+            //int m = int.Parse(Console.ReadLine());
+
+            //int counter;
+            //int result;
+
+            //do
+            //{
+            //    if (n <= 1)
+            //    {
+            //        break;
+            //    }
+            //    m *= n;
+            //    n--;
+            //} while (true); //interesting! this loop has a semi-colon at the end.
+
+
+            //Console.WriteLine(m);
+
+
+            int i;
+
+            for(i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
